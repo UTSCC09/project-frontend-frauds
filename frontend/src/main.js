b11c77d5d86a13dbebc72e0b0b6dc6403282d5f9
Payam Yektamaram <payamyek@gmail.com>
2022-07-01 13:18:30 -0400

    feat: elements plus framework

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(ElementPlus);
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
