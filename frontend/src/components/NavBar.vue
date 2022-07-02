5ae17d6d1d905dd70e38397a2a008b2b49e06d6c
Payam Yektamaram <payamyek@gmail.com>
2022-07-02 18:40:37 -0400

    feat: flight search form skeleton

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const activeIndex = ref("1");
+const handleSelect = (key, keyPath) => {
+  console.log(key, keyPath);
+};
+</script>
+
+<template>
+  <el-menu
+    :default-active="activeIndex"
+    class="app-nav-bar"
+    mode="horizontal"
+    background-color="transparent"
+    text-color="white"
+    @select="handleSelect"
+  >
+    <el-menu-item index="1">Home</el-menu-item>
+    <el-menu-item index="2">Bookings</el-menu-item>
+    <el-menu-item index="3">Credits</el-menu-item>
+  </el-menu>
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/dark/css-vars.css";
 import router from "./router";
 
 const app = createApp(App);
