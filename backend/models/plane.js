3c271f107ffebc104a60ea681706acbe53394b36
Payam Yektamaram <payamyek@gmail.com>
2022-07-08 22:56:43 -0400

    feat: dynamic form airline, airport data

--- backend/models/plane.js
+++ backend/models/plane.js
@@ -1,46 +1,4 @@
 import mongoose from "mongoose";
-const { Schema } = mongoose;
-import { generateSearch } from "../api/helpers/index.js";
+import { PlaneSchema } from "../schemas/index.js";
 
-const Plane = new Schema(
-  {
-    name: String,
-    iata: String,
-    icao: String,
-    passengerCapacity: Number,
-    seats: [[]],
-  },
-  {
-    statics: {
-      async paginate(page = 0, limit = 10) {
-        const count = await this.estimatedDocumentCount({});
-        const docs = await this.find({})
-          .sort({ name: 1 })
-          .skip(page * limit)
-          .limit(limit);
-
-        return { count, docs };
-      },
-      async search(query, match, include, exclude, limit = 10) {
-        // no results
-        if (!query) return { data: [] };
-
-        // conduct search
-        const docs = await this.find(
-          { $text: { $search: query } },
-          {
-            score: { $meta: "textScore" },
-            ...generateProjection(include, exclude),
-          }
-        )
-          .sort({ score: { $meta: "textScore" } })
-          .limit(limit);
-
-        // return results
-        return { data: docs };
-      },
-    },
-  }
-);
-
-export default mongoose.model("Plane", Plane);
+export default mongoose.model("Plane", PlaneSchema);
---
+++ backend/schemas/airlineSchema.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const Airline = new Schema(
+  {
+    airlineId: String,
+    name: String,
+    alias: String,
+    iata: String,
+    icao: String,
+    callsign: String,
+    country: String,
+    active: String,
+  },
+  {
+    statics: {
+      async paginate(page = 0, limit = 10) {
+        const count = await this.estimatedDocumentCount({});
+        const docs = await this.find({})
+          .sort({ name: 1 })
+          .skip(page * limit)
+          .limit(limit);
+        return { count, docs };
+      },
+      async search(query, match, include, exclude, limit = 10) {
+        // no results
+        if (!query) return { data: [] };
+
+        // conduct search
+        const docs = await this.find(
+          { $text: { $search: query } },
+          {
+            score: { $meta: "textScore" },
+            ...generateProjection(include, exclude),
+          }
+        )
+          .sort({ score: { $meta: "textScore" } })
+          .limit(limit);
+
+        // return results
+        return { data: docs };
+      },
+    },
+  }
+);
+
+export default Airline;
