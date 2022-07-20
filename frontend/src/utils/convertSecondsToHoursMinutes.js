abebc2af6998d67f5e2e7c0736523e95324eaed9
Payam Yektamaram <payamyek@gmail.com>
2022-07-20 01:00:09 -0400

    feat: complete redesign of flight search results

--- frontend/src/utils/convertSecondsToHoursMinutes.js
+++ frontend/src/utils/convertSecondsToHoursMinutes.js
@@ -1,5 +1,5 @@
 export default (seconds) => {
-  return `${Math.floor(seconds / 3600)} H ${Math.floor(
+  return `${Math.floor(seconds / 3600)}H ${Math.floor(
     (seconds / 3600 - Math.floor(seconds / 3600)) * 60
-  )} M`;
+  )}M`;
 };
