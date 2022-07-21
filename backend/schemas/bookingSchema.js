caf9fdcd5c2f60db41b4516ed2512b0c0228f36c
Payam Yektamaram <payamyek@gmail.com>
2022-07-20 21:08:26 -0400

    feat: roundtrip bookings backend integration

--- backend/schemas/bookingSchema.js
+++ backend/schemas/bookingSchema.js
@@ -3,6 +3,31 @@ import mongoose from "mongoose";
 import { Flight } from "../models/index.js";
 const { Schema, Types } = mongoose;
 
+// validates flight seats
+const validateSeatHelper = async (flight) => {
+  // locate departure flight
+  const doc = await Flight.findOne({
+    _id: Types.ObjectId(flight.flightId),
+  });
+
+  // invalid departure flight
+  if (doc === null) throw createError(400, "Departure flight doesn't exist");
+
+  // get seat map
+  const seatMap = [...doc.equipmentListData.seats];
+  const seatValue = seatMap[flight.seat.x][flight.seat.y];
+
+  // cannot reserve barrier seat
+  if (seatValue === -1)
+    throw createError(400, "Chosen seat is not a valid seat");
+  else if (seatValue === 0) throw createError(400, "Seat already reserved");
+
+  // update seats for departure flight as booked
+  seatMap[flight.seat.x][flight.seat.y] = 0;
+
+  return { seats: seatMap, doc };
+};
+
 const BookingSchema = new Schema(
   {
     userId: String,
@@ -68,33 +93,24 @@ const BookingSchema = new Schema(
         currency,
         returnFlight = null
       ) {
-        // locate departure flight
-        const docDeparture = await Flight.findOne({
-          _id: Types.ObjectId(departureFlight.flightId),
-        });
+        // validate seats for these flights
+        const { seats: depSeats, doc: depDoc } = await validateSeatHelper(
+          departureFlight
+        );
+        const { seats: returnSeats, doc: returnDoc } = await validateSeatHelper(
+          returnFlight
+        );
 
-        // invalid departure flight
-        if (docDeparture === null)
-          throw createError(400, "Departure flight doesn't exist");
-
-        // get seat map
-        const seatMap = [...docDeparture.equipmentListData.seats];
-        const seatValue =
-          seatMap[departureFlight.seat.x][departureFlight.seat.y];
-
-        // cannot reserve barrier seat
-        if (seatValue === -1)
-          throw createError(400, "Chosen seat is not a valid seat");
-        else if (seatValue === 0)
-          throw createError(400, "Seat already reserved");
-
-        // update seats for departure flight as booked
-        seatMap[departureFlight.seat.x][departureFlight.seat.y] = 0;
-
-        // update flights seats data
+        // reserve departure seats
         await Flight.updateOne(
-          { _id: Types.ObjectId(docDeparture._id) },
-          { $set: { "equipmentListData.seats": seatMap } }
+          { _id: Types.ObjectId(depDoc._id) },
+          { $set: { "equipmentListData.seats": depSeats } }
+        );
+
+        // reserve return seats
+        await Flight.updateOne(
+          { _id: Types.ObjectId(returnDoc._id) },
+          { $set: { "equipmentListData.seats": returnSeats } }
         );
 
         // create booking
