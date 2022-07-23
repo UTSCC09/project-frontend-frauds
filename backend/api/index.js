2df2d4ee5f7522a3a1af72f50ad96d92cb29948f
Payam Yektamaram <payamyek@gmail.com>
2022-07-23 18:37:25 -0400

    feat: queue with sandboxed processors

--- backend/api/index.js
+++ backend/api/index.js
@@ -8,6 +8,7 @@ import {
   routeRoutes,
   flightRoutes,
   bookingRoutes,
+  bullBoardRoutes,
 } from "./routes/index.js";
 
 import bodyParser from "body-parser";
@@ -34,6 +35,7 @@ app.use("/api/airports", airportRoutes);
 app.use("/api/routes", routeRoutes);
 app.use("/api/flights", flightRoutes);
 app.use("/api/bookings", bookingRoutes);
+app.use("/admin/queues", bullBoardRoutes);
 
 // ---------- THIS MUST BE LAST DO NOT TOUCH  ----------
 app.use(errorHandlerMiddleware);
--- backend/config/index.js
+++ backend/config/index.js
@@ -6,16 +6,23 @@ dotenv.config();
 // app configs
 const config = {};
 
+// META CONFIGS
 config.AIRTORONTO_BACKEND_URL = process.env.AIRTORONTO_BACKEND_URL;
 config.APP_ENV = process.env.APP_ENV;
 config.SERVER_PORT = Number.parseInt(process.env.SERVER_PORT);
 
+// MONGO CONFIGS
 config.MONGO_DB_USER = process.env.MONGO_DB_USER;
 config.MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
 config.MONGO_DB_DATABASE = process.env.MONGO_DB_DATABASE;
 config.MONGO_DB_HOST = process.env.MONGO_DB_HOST;
 config.MONGO_DB_PORT = process.env.MONGO_DB_PORT;
 config.MONGO_DB_URI = `mongodb://${config.MONGO_DB_USER}:${config.MONGO_DB_PASSWORD}@${config.MONGO_DB_HOST}:${config.MONGO_DB_PORT}`;
+
+// REDIS CONFIGS
+config.REDIS_HOST = process.env.REDIS_HOST;
+config.REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+config.REDIS_PORT = Number.parseInt(process.env.REDIS_PORT);
 
 // export app config
 export default config;
