c6de111119c047ee47fedf5319ffffa4c3bbefac
Payam Yektamaram <payamyek@gmail.com>
2022-07-26 23:41:09 -0400

    refactor: removed unused code and params

--- frontend/src/services/plane.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import config from "../../config";
-
-export async function searchPlanes(query) {
-  return axios.post(`${config.BACKEND_URL}/api/planes/search`, {
-    query,
-    fields: ["name", "iata"],
-  });
-}
