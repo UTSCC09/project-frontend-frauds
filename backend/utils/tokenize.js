32b39ec9bfd79c63bb623cb349c135ab5149544a
Payam Yektamaram <payamyek@gmail.com>
2022-07-26 22:22:03 -0400

    refactor: delete unused function

--- backend/utils/tokenize.js
+++
@@ -1,9 +0,0 @@
-export default function tokenize(text) {
-  if (text.length <= 1) return [];
-
-  // tokenize query
-  return text
-    .trim()
-    .split(" ")
-    .filter((x) => x.length > 1);
-}
