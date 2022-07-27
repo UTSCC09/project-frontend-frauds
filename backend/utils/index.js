88826060beea15634c6b892d71f8309b93333dd4
Payam Yektamaram <payamyek@gmail.com>
2022-07-26 22:22:03 -0400

    refactor: delete unused function

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,12 +1,10 @@
 import logger from "./logger.js";
-import tokenize from "./tokenize.js";
 import timestampGetEndOfDay from "./timestampGetEndOfDay.js";
 import timestampGetStartOfDay from "./timestampGetStartOfDay.js";
 import generatePaginationLinks from "./generatePaginationLinks.js";
 
 export {
   logger,
-  tokenize,
   timestampGetEndOfDay,
   timestampGetStartOfDay,
   generatePaginationLinks,
