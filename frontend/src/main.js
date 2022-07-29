c4e8a030178bf3efbba23c3340977e684cd945e2
Payam Yektamaram <payamyek@gmail.com>
2022-07-29 14:13:55 -0400

    feat: ts particles animations for page not found and purchase

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,12 @@ import App from "./App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
+import Particles from "vue3-particles";
 import router from "./router";
 
 const app = createApp(App);
 
+app.use(Particles);
 app.use(ElementPlus);
 app.use(router);
 
