01a0252770283e588b7ea231df5ab488e58bfb58
Ling Su <flyingzambie0309@gmail.com>
2022-07-29 23:30:26 -0400

    JSHINT + Removed Obsolete passportJS code

--- backend/api/routes/oauthRoutes.js
+++ backend/api/routes/oauthRoutes.js
@@ -1,15 +1,8 @@
 import express from "express";
-import passport from "passport";
-import { strategy } from "../helpers/index.js";
 import user from "../../models/user.js";
 import asyncHandler from "express-async-handler";
 
 const router = express.Router();
-strategy();
-
-function isLoggedIn(req, res, next) {
-  req.user ? next() : res.sendStatus(401);
-}
 
 router.post(
   "/testUserDBCreate",
@@ -35,40 +28,5 @@ router.post(
     res.json(await user.findUser(email));
   })
 );
-
-router.get(
-  "/google/signin",
-  passport.authenticate("google", { scope: ["email", "profile"] })
-);
-
-router.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    successRedirect: "/auth/google/success",
-    failureRedirect: "/auth/google/failure",
-  })
-);
-
-router.get("/google/success", isLoggedIn, (req, res) => {
-  console.log(req.user);
-  res.send(
-    "hello " +
-      req.user.displayName +
-      ", your email is: " +
-      req.user.emails[0].value
-  );
-});
-
-router.get("/google/failure", isLoggedIn, (req, res) => {
-  res.sendStatus(401);
-});
-
-router.get("/google/signout", (req, res) => {
-  req.logout(function (err) {
-    if (err) return next(err);
-    req.session.destroy();
-    res.redirect("/");
-  });
-});
 
 export default router;
