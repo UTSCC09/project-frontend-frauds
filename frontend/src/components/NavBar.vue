1c4247465127f43ad2fd3643f0fcae5daa118082
Ling Su <flyingzambie0309@gmail.com>
2022-08-01 12:16:57 -0400

    Prettier

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -13,7 +13,13 @@ import {
 import { useAuth0 } from "@auth0/auth0-vue";
 import { getUserInfo } from "../services/user";
 
-const { loginWithRedirect: login, user, isAuthenticated, logout, isLoading } = useAuth0();
+const {
+  loginWithRedirect: login,
+  user,
+  isAuthenticated,
+  logout,
+  isLoading,
+} = useAuth0();
 
 async function loginWithRedirect() {
   await login();
@@ -29,13 +35,13 @@ const adminPriv = ref(false);
 const userInfo = ref({});
 const loading = ref(true);
 
-watch(user, async (loadingNew) =>  { // I don't understand why user.email returns undefined but this works.
+watch(user, async (loadingNew) => {
+  // I don't understand why user.email returns undefined but this works.
   userInfo.value = await getUserInfo(loadingNew.email);
   adminPriv.value = ["user", "admin"].every((val) =>
-      userInfo.value.role.includes(val)
+    userInfo.value.role.includes(val)
   );
   loading.value = false;
-
 });
 
 onMounted(async () => {
@@ -50,7 +56,6 @@ onMounted(async () => {
   } else if (currentUrl.endsWith("/user-profile")) {
     activeIndex.value = "5";
   }
-
 });
 </script>
 
