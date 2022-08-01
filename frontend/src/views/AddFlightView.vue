494538314be145557b3859d186591d9c7a43a99b
Ling Su <flyingzambie0309@gmail.com>
2022-08-01 10:20:14 -0400

    Added frontend role based guard

--- frontend/src/views/AddFlightView.vue
+++ frontend/src/views/AddFlightView.vue
@@ -1,11 +1,26 @@
 <script setup>
 import AddFlight from "../components/AddFlight.vue";
 import PageTitle from "../components/PageTitle.vue";
+import {onMounted, ref} from "vue";
+import {getUserInfo} from "../services/user";
+
+const adminPriv = ref(false);
+const userInfo = ref({})
+const loading = ref(true);
+onMounted(async () => {
+  userInfo.value = await getUserInfo();
+  adminPriv.value = ["user", "admin"].every((val) => userInfo.value.role.includes(val));
+  loading.value = false;
+});
+
 </script>
 
 <template>
-  <main>
+  <main v-if="!loading && adminPriv">
     <PageTitle title="Add Flight" />
     <AddFlight />
+  </main>
+  <main v-if="!loading && !adminPriv">
+    <h1>Please ensure you have admin privilege</h1>
   </main>
 </template>
